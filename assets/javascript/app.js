2197422b967de6c1e055d7c66c2df6d7c018b90c
Kyle Laster <kyle.laster@gmail.com>
2019-01-29 10:26:32 -0600

    added ajax call

---
+++ assets/javascript/app.js
@@ -0,0 +1,93 @@
+
+$('#form').on('submit', function() {
+    event.preventDefault();
+    getmoviePoster(getValue());
+    getYoutubeTrailer(getValue());
+    getYoutubeReview(getValue());
+});
+
+
+function getValue() {
+    if ($('#input').val() !== 'Enter Movie Title') {
+      var movieTitle = $('#input').val();  
+    }
+    return movieTitle
+}
+
+
+function getmoviePoster(movieTitle) {
+    if (movieTitle !== undefined) {
+    $.get(`https://www.omdbapi.com/?t=${movieTitle}&apikey=fcc96c64`,
+    function(response) {
+        console.log(response.Poster)
+        $('#theImg').attr('src', response.Poster)
+    })
+    };
+}
+
+
+
+var posterSource = getmoviePoster(getValue());
+
+
+/// The following are for youtube api
+var apiKey = 'AIzaSyDfh0vTr9C2bILx8r9o3PAkO_87tXlmBu8'
+
+////////////////////////////////////////////////////
+//following are for youtube movie trailers
+function getYoutubeTrailer(movieTitle) {
+    if (movieTitle !== undefined) {
+    $.get(`https://www.googleapis.com/youtube/v3/search?maxResults=25&part=snippet&q=${movieTitle} trailer&key=AIzaSyDfh0vTr9C2bILx8r9o3PAkO_87tXlmBu8`,
+    function(response) {
+        console.log(response.items)
+        var idArray = []
+        response.items.forEach(function(cur) {
+            idArray.push(cur.id.videoId);
+        });
+        function printVids(youtubeArray){
+            console.log('akuna');
+            return youtubeArray.map(function(cur, index) {
+             $(`.iframe${index}`).attr('src', `https://www.youtube.com/embed/${cur}`)
+         });
+    }         printVids(idArray);
+});
+    };
+};
+
+function publishYoutube(youtubeArray) {
+   return youtubeArray.map(function(cur, index) {
+    $(`.iframe${index}`).attr('src', `https://www.youtube.com/embed/${cur}`)
+});
+};
+getYoutubeTrailer(getValue());
+
+//////////////////////////////////////////////////////
+//Following is for youtube reviews
+
+function getYoutubeReview(movieTitle) {
+    $.get(`https://www.googleapis.com/youtube/v3/search?maxResults=25&part=snippet&q=${movieTitle} review&key=AIzaSyDfh0vTr9C2bILx8r9o3PAkO_87tXlmBu8`,
+    function(response) {
+        console.log(response.items)
+        var idArray = []
+        response.items.forEach(function(cur) {
+            idArray.push(cur.id.videoId);
+        });
+        function printVids(youtubeArray){
+            console.log('akuna');
+            return youtubeArray.map(function(cur, index) {
+             $(`.review${index}`).attr('src', `https://www.youtube.com/embed/${cur}`)
+         });
+    }         
+    printVids(idArray);
+});
+};
+
+function publishYoutube(youtubeArray) {
+   return youtubeArray.map(function(cur, index) {
+    $(`.iframe${index}`).attr('src', `https://www.youtube.com/embed/${cur}`)
+});
+};
+getYoutubeReview(movieTitle);
+
+
+
